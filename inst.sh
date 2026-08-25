649d0f758f826505e9d848087e20ab9a66f49ccc
akeda <akeda2@gmail.com>
2026-08-25 18:45:32 +0200

    copy to /usr/local/bin for sudo

--- inst.sh
+++ inst.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # First run: cargo build --release
 cargo install --path .
-#test -f target/release/ebba && cp target/release/ebba ~/.local/bin/ebba || echo "No target binary. Did you run cargo build --release?"
+test -f target/release/ebba && sudo cp target/release/ebba /usr/local/bin/ebba || echo "No target binary. Did you run cargo build --release?"
