9631cb5b6307e5b9991157790a1c685128c4b8a9
akeda <akeda2@gmail.com>
2026-08-25 18:41:57 +0200

    installer using cargo install

--- inst.sh
+++ inst.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # First run: cargo build --release
-test -f target/release/ebba && cp target/release/ebba ~/.local/bin/ebba || echo "No target binary. Did you run cargo build --release?"
+cargo install --path .
+#test -f target/release/ebba && cp target/release/ebba ~/.local/bin/ebba || echo "No target binary. Did you run cargo build --release?"
