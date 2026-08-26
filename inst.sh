21f3756c03624d5a4ab8eb71c83b662640e67004
akeda <akeda2@gmail.com>
2026-08-26 07:14:50 +0200

    gfff in inst

--- inst.sh
+++ inst.sh
@@ -4,3 +4,4 @@ cargo install --path .
 test -f target/release/ebba && sudo cp target/release/ebba /usr/local/bin/ebba || echo "No target binary. Did you run cargo build --release?"
 which ebba
 sudo which ebba
+which gb && gb -I ebba.yaml -a -w
